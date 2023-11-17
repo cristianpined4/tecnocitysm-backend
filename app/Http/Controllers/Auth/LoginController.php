e6711b57db946bfd8b677705a27450ee88145705
Cristian Pineda <cristian.pineda.1997@outlook.com>
2023-11-16 21:41:24 -0600

    added auth full with passpost laravel

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -36,5 +40,60 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function login(Request $req)
+    {
+        $user = User::with('rol')->where('email', $req->email)->first();
+        if (!$user) {
+            return response()->json([
+                'message' => 'No existe un usuario con ese correo',
+                'success' => false
+            ]);
+        }
+
+        if (!Hash::check($req->password, $user->password)) {
+            return response()->json([
+                'message' => 'Contraseña incorrecta',
+                'success' => false
+            ]);
+        }
+
+        $user->token_web = $req->token_web;
+        $user->update();
+        Auth::login($user);
+        return response()->json([
+            'message' => 'Sesión iniciada correctamente',
+            'success' => true,
+            'token' => $user->token_web
+        ]);
+    }
+
+    public function check()
+    {
+        if (auth()->user()) {
+            return response()->json([
+                'message' => 'Sesión iniciada',
+                'success' => true,
+                'user' => auth()->user()
+            ]);
+        } else {
+            return response()->json([
+                'message' => 'Sesión no iniciada',
+                'success' => false
+            ]);
+        }
+    }
+
+    public function logout()
+    {
+        $current_User = User::find(auth()->user()->id);
+        $current_User->token_web = null;
+        $current_User->update();
+        Auth::logout();
+        return response()->json([
+            'message' => 'Sesión cerrada correctamente',
+            'success' => true
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Roles;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +18,36 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get("init", function () {
+
+    $rol = roles::find(1);
+    if ($rol) {
+        return response()->json([
+            "message" => "Base de datos ya inicializada",
+            "success" => false
+        ]);
+    }
+
+    $rol = new Roles();
+    $rol->name = "Administador";
+    $rol->save();
+    $rol = new Roles();
+    $rol->name = "Usuario";
+    $rol->save();
+    $user = new User();
+    $user->name = "admin";
+    $user->username = "admin";
+    $user->email = "admin@admin.com";
+    $user->password = Hash::make("admin");
+    $user->rol_id = 1;
+    $user->save();
+
+    return response()->json([
+        "message" => "Base de datos inicializada correctamente",
+        "success" => true
+    ]);
 });
 
 Auth::routes();
